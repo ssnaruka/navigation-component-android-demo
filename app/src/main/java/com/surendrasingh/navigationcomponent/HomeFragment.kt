fe03e9173fddd0fa5f4f63078f2930c6130e508a
ssnaruka <surendranaruka33@gmail.com>
2022-09-25 23:49:22 +0530

    -create navigation graph

---
+++ app/src/main/java/com/surendrasingh/navigationcomponent/HomeFragment.kt
@@ -0,0 +1,10 @@
+package com.surendrasingh.navigationcomponent
+
+import androidx.fragment.app.Fragment
+
+/**
+ * Created by Surendra Singh on 25-09-2022
+ */
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+}
