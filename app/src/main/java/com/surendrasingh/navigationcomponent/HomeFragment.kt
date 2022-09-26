9ca7e7c227bbd692abc33150f62ce186b3df7868
ssnaruka <surendranaruka33@gmail.com>
2022-09-26 23:56:47 +0530

    -navigate one fragment to another using navController -add data binding

--- app/src/main/java/com/surendrasingh/navigationcomponent/HomeFragment.kt
+++ app/src/main/java/com/surendrasingh/navigationcomponent/HomeFragment.kt
@@ -1,10 +1,34 @@
 package com.surendrasingh.navigationcomponent
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.surendrasingh.navigationcomponent.databinding.FragmentHomeBinding
 
 /**
  * Created by Surendra Singh on 25-09-2022
  */
-class HomeFragment : Fragment(R.layout.fragment_home) {
+class HomeFragment : Fragment() {
 
+    private var binding: FragmentHomeBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        setupView()
+        return binding?.root
+    }
+
+    private fun setupView() {
+        binding?.btnLogin?.setOnClickListener {
+            val action = HomeFragmentDirections.actionHomeFragmentToLoginFragment()
+            findNavController().navigate(action)
+        }
+    }
 }
---
+++ app/src/main/java/com/surendrasingh/navigationcomponent/LoginFragment.kt
@@ -0,0 +1,9 @@
+package com.surendrasingh.navigationcomponent
+
+import androidx.fragment.app.Fragment
+
+/**
+ * Created by Surendra Singh on 25-09-2022
+ */
+class LoginFragment :Fragment(R.layout.fragment_login) {
+}
